59fca423c5fda65922fc3441b8a3b14d91855621
JoramirJr <joramirjunior@gmail.com>
2024-06-09 15:56:55 -0300

    1st commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::io::Write;
+use std::process;
+
+mod token_type;
+struct Main {
+    args: Vec<String>,
+    had_error: bool,
+}
+
+impl Main {
+    fn main(mut self) {
+        if self.args.len() > 1 {
+            println!("Usage: jlox [script]");
+            process::exit(64);
+        } else if self.args.len() == 1 {
+            Self::run_file(&mut self);
+        } else {
+            Self::run_prompt(self);
+        }
+    }
+    fn run_file(&self) {
+        if self.had_error {
+            process::exit(65);
+        } else {
+            let _ = fs::read_to_string(&self.args[0]).expect_err("File reading successful");
+        }
+    }
+
+    fn run_prompt(mut self) -> io::Error {
+        let mut input = String::new();
+        let mut std_out_handler: io::Stdout = io::stdout();
+
+        loop {
+            let _ = std_out_handler.write_all("> ".as_bytes());
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    Self::run(&input, &mut std_out_handler);
+                    self.had_error = false;
+                }
+                //Not sure if ctlr+D, to exit the program, generates an error response
+                Err(error) => return error,
+            }
+        }
+    }
+
+    fn run(source: &String, std_out_handler: &mut io::Stdout) {
+        source.split(" ").for_each(|token| {
+            let _ = std_out_handler.write_all(token.as_bytes());
+        });
+    }
+
+    fn error(line: usize, message: String) {
+        Self::report(line, String::new(), message);
+    }
+
+    fn report(line: usize, location: String, message: String) {
+        let err_msg = format!("[line {}] Error {}: {}", line, location, message);
+        let mut err_out_handler = io::stderr();
+        let _ = err_out_handler.write_all(err_msg.as_bytes());
+    }
+}
+
+fn main() {
+    let _ = Main {
+        args: env::args().collect(),
+        had_error: false,
+    };
+}

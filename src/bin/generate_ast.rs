1291dc266dd2f6845850b1f66fd6281f051c136e
Joramir da Silva <joramir_ext@powerrev.com.br>
2024-10-07 08:10:53 -0300

    generating the AST generator; still not tested

--- src/bin/generate_ast.rs
+++ src/bin/generate_ast.rs
@@ -1,4 +1,5 @@
-use std::process;
+use std::io::Write;
+use std::{io, process};
 
 pub struct Main {
     args: Vec<String>,
@@ -13,10 +14,40 @@ impl Main {
         }
         let output_dir: &_ = &self.args[2];
         Self::define_ast(
-            output_dir, 
-            "Expr", 
-            Vec::from(["Binary: Expr left, Token operator, Expr right", "Grouping: Expr expression", "Literal: Object value", "Unary: Token operator, Expr right"])
+            output_dir,
+            "Expr",
+            Vec::from([
+                "Binary: Expr left, Token operator, Expr right",
+                "Grouping: Expr expression",
+                "Literal: Object value",
+                "Unary: Token operator, Expr right",
+            ]),
         )
     }
-    fn define_ast(output_dir: &String, basename: &str, types: Vec<&str>){}
+    fn define_ast(output_dir: &String, basename: &str, types: Vec<&str>) {
+        let path = [output_dir, "/", basename, ".rs"].concat();
+
+        for t in types {
+            let struct_name_and_fields = t.split_once(":").unwrap();
+            let structname = struct_name_and_fields.0.trim();
+            let fields = struct_name_and_fields.1.trim();
+        }
+    }
+
+    fn define_type(basename: &str, structname: &str, fieldList: &str) {
+        let mut std_out_handler: io::Stdout = io::stdout();
+
+        let _ = std_out_handler.write_all(
+            ["struct", structname, "extends", basename]
+                .concat()
+                .as_bytes(),
+        );
+        let _ = std_out_handler.write_all(["     ", "(", fieldList, ")", "{"].concat().as_bytes());
+        let fields = fieldList.split(", ");
+
+        for field in fields {
+            let _ = std_out_handler.write_all(["     final", field, ";"].concat().as_bytes());
+        }
+        let _ = std_out_handler.write_all("  }".as_bytes());
+    }
 }

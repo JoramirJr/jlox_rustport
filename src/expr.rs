dfcaaa6641dde4a11e436bad3e73f554357dd8a2
JoramirJr <joramirjunior@gmail.com>
2025-01-20 11:33:14 -0300

    continuing the parser (parsing expressions. context-free grammar)

--- src/expr.rs
+++ src/expr.rs
@@ -1,69 +1,69 @@
 pub mod expr {
-    use crate::token_type::Token;
+use crate::token_type::Token;
 
-    trait ExpressionBehaviors {
-        fn interpret(&self) -> ();
-        fn resolve(&self) -> ();
-        fn analyze(&self) -> ();
-    }
+trait ExpressionBehaviors {
+  fn interpret(&self) -> ();
+  fn resolve(&self) -> ();
+  fn analyze(&self) -> ();
+}
 
-    struct Binary {
-        left: String,
-        operator: Token<String>,
-        right: String,
-    }
-    struct Grouping {
-        expression: String,
-    }
-    struct Literal<T> {
-        value: Option<T>,
-    }
-    struct Unary {
-        operator: Token<String>,
-        right: String,
-    }
-    impl ExpressionBehaviors for Binary {
-        fn interpret(&self) -> () {
-            ()
-        }
-        fn resolve(&self) -> () {
-            ()
-        }
-        fn analyze(&self) -> () {
-            ()
-        }
-    }
-    impl ExpressionBehaviors for Grouping {
-        fn interpret(&self) -> () {
-            ()
-        }
-        fn resolve(&self) -> () {
-            ()
-        }
-        fn analyze(&self) -> () {
-            ()
-        }
-    }
-    impl ExpressionBehaviors for Literal<String> {
-        fn interpret(&self) -> () {
-            ()
-        }
-        fn resolve(&self) -> () {
-            ()
-        }
-        fn analyze(&self) -> () {
-            ()
-        }
-    }
-    impl ExpressionBehaviors for Unary {
-        fn interpret(&self) -> () {
-            ()
-        }
-        fn resolve(&self) -> () {
-            ()
-        }
-        fn analyze(&self) -> () {
-            ()
-        }
-    }
+struct Binary {
+    left: String,
+    operator: Token,
+    right: String,
+}
+struct Grouping {
+    expression: String,
+}
+struct Literal<T> {
+    value: Option<T>,
+}
+struct Unary {
+    operator: Token,
+    right: String,
+}
+impl ExpressionBehaviors for Binary { 
+  fn interpret(&self) -> () {
+                        ()
+                    }
+  fn resolve(&self) -> () {
+                        ()
+                    }
+  fn analyze(&self) -> () {
+                        ()
+                    }
+}
+impl ExpressionBehaviors for Grouping { 
+  fn interpret(&self) -> () {
+                        ()
+                    }
+  fn resolve(&self) -> () {
+                        ()
+                    }
+  fn analyze(&self) -> () {
+                        ()
+                    }
+}
+impl ExpressionBehaviors for Literal<T> { 
+  fn interpret(&self) -> () {
+                        ()
+                    }
+  fn resolve(&self) -> () {
+                        ()
+                    }
+  fn analyze(&self) -> () {
+                        ()
+                    }
+}
+impl ExpressionBehaviors for Unary { 
+  fn interpret(&self) -> () {
+                        ()
+                    }
+  fn resolve(&self) -> () {
+                        ()
+                    }
+  fn analyze(&self) -> () {
+                        ()
+                    }
+}
 }
--- src/parser.rs
+++ src/parser.rs
@@ -14,12 +14,38 @@ impl Parser {
     }
     fn equality() {
         let mut expr = Self::comparison();
-        while Self::match_expr([TokenType::BangEqual, TokenType::EqualEqual]) {
+        while Self::match_expr([TokenType::BangEqual, TokenType::EqualEqual] ) {
             let operator = Self::previous();
             let right = Self::comparison();
             expr = expr
         }
     }
     fn comparison() {}
-    fn match_expr(types: &[TokenType]) {}
+    fn match_expr(types: &[TokenType]) -> bool {
+        types.iter().any(|t| { 
+            if Self::check(t) {
+                Self::advance();
+                return true;
+            } else {
+                return false;
+            }
+         });
+         false
+    }
+    fn check(t_type: &TokenType) -> bool {
+        if(Self::is_at_end()) {
+            false
+        } else {
+            Self::peek().type == t_type
+        }
+    }
+    fn advance() {
+        if(Self::is_at_end()) {
+            self.current += 1;
+        }
+        return Self::previous()
+    }
+    fn is_at_end() {}
+    fn peek() {}
+    fn previous(){}
 }

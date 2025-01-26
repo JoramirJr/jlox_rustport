33e369ef87fb0ac5bd98930df94c69de08275620
JoramirJr <joramirjunior@gmail.com>
2025-01-26 13:22:27 -0300

    em progresso - implementação de parser para context-free grammar (5)

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
+use crate::expr::expr::{Binary, Grouping, Literal, Unary};
 use crate::token_type::*;
-use crate::expr::expr::Binary;
 
 struct Parser {
     tokens: Vec<Token<String>>,
@@ -14,37 +14,115 @@ impl Parser {
         Self::equality()
     }
     fn equality(&mut self) -> Binary {
-        let mut expr = Self::comparison();
-        while Self::match_expr(self, [TokenType::BangEqual, TokenType::EqualEqual] ) {
+        let mut expr = Self::comparison(self);
+        while Self::match_expr(self, &[TokenType::BangEqual, TokenType::EqualEqual]) {
             let operator = Self::previous(self);
-            let right = Self::comparison();
-            expr = Binary { left: expr, operator, right: right }
+            let right = Self::comparison(self);
+            expr = Binary {
+                left: expr,
+                operator,
+                right: right,
+            }
         }
         expr
     }
-    fn comparison() -> Binary {
-        
+    fn comparison(&mut self) -> Binary {
+        let expr = Self::term();
+
+        while Self::match_expr(
+            &mut self,
+            &[
+                TokenType::Greater,
+                TokenType::GreaterEqual,
+                TokenType::Less,
+                TokenType::LessEqual,
+            ],
+        ) {
+            let operator = Self::previous(&self);
+            let right = Self::term();
+            let expr = Binary { left: expr, operator: operator, right: right }
+        }
+
+        return expr;
     }
-    fn match_expr(&mut self, types: [TokenType; 2]) -> bool {
-        types.iter().any(|t| { 
+    fn term() {
+        let expr = Self::factor();
+
+        while Self::match_expr(&mut self, &[TokenType::Minus, TokenType::Plus]) {
+            let operator = Self::previous(&self);
+            let right = Self::factor();
+            let expr = Binary { left: expr, operator: operator, right: right }
+        }
+
+        return expr;
+
+    }
+    fn factor() {
+        let expr = Self::unary();
+
+        while Self::match_expr(&mut self, &[TokenType::Slash, TokenType::Star]) {
+            let operator = Self::previous(&self);
+            let right = Self::unary();
+            let expr = Binary { left: expr, operator: operator, right: right }
+        }
+
+        return expr;
+
+    }
+    fn unary(){
+        if Self::match_expr(&mut self, &[TokenType::Bang, TokenType::Minus]) {
+            let operator = Self::previous(&self);
+            let expr = Self::unary();
+            return Unary { operator: operator, right: right } 
+        }
+        return; Self::primary();
+    }
+    fn primary(){
+        if Self::match_expr(&mut self, &[TokenType::False]) {
+            Literal { value: false }
+        }
+        if Self::match_expr(&mut self, &[TokenType::True]) {
+            Literal { value: true }
+        }
+        if Self::match_expr(&mut self, &[TokenType::Nil]) {
+            Literal { value: () }
+        }
+
+        if Self::match_expr(&mut self, &[TokenType::Number, TokenType::String]) {
+            Literal { value: Self::previous(&self).literal }
+        }
+
+        if Self::match_expr(&mut self, &[TokenType::LeftParen]) {
+            let expr = Self::expression();
+            Self::consume(TokenType::RightParen, "Expect ')' after expression");
+            Grouping { expression: expr }
+        }
+    }
+    fn consume (){}
+    fn match_expr(&mut self, types: &[TokenType]) -> bool {
+        let check = types.iter().any(|t| {
             if Self::check(self, t) {
                 Self::advance(self);
                 return true;
             } else {
                 return false;
             }
-         });
-         false
+        });
+        if check {
+            check
+        } else {
+            false
+        }
     }
     fn check(&self, t_type: &TokenType) -> bool {
-        if(Self::is_at_end(self)) {
+        if (Self::is_at_end(self)) {
             false
         } else {
             Self::peek(self).ttype == *t_type
         }
     }
     fn advance(&mut self) -> Token<String> {
-        if Self::is_at_end(self)  {
+        if Self::is_at_end(self) {
             self.current += 1;
         }
         Self::previous(self)

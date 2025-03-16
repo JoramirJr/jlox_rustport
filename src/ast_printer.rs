22ef1ead5b18a61ea866b3c2fc56487af6451aec
JoramirJr <joramirjunior@gmail.com>
2025-03-16 14:13:13 -0300

    in progress - AST printer finished

--- src/ast_printer.rs
+++ src/ast_printer.rs
@@ -3,43 +3,26 @@ use crate::token_type::LiteralType;
 pub struct AstPrinter();
 
 impl AstPrinter {
-    pub fn print(expr: ExpressionType) -> String {
+    pub fn print(expr: &ExpressionType) -> String {
         match expr {
             ExpressionType::BinaryExpr(expr) => {
-                Self::parenthesize(expr.operator.lexeme, [&expr.left, &expr.right].to_vec())
+                Self::parenthesize(&expr.operator.lexeme, [&expr.left, &expr.right].to_vec())
             }
             ExpressionType::GroupingExpr(expr) => {
-                Self::parenthesize("group".to_string(), [&expr.expression].to_vec())
+                Self::parenthesize(&"group".to_string(), [&expr.expression].to_vec())
             }
-            ExpressionType::LiteralExpr(expr) => match expr.value {
+            ExpressionType::LiteralExpr(expr) => match &expr.value {
                 LiteralType::Nil => "nil".to_string(),
-                _ => expr.value.to_string(),
+                LiteralType::String(value) => value.clone(),
+                LiteralType::Bool(value) => value.to_string().clone(),
+                LiteralType::F32(value) => value.to_string(),
             },
             ExpressionType::UnaryExpr(expr) => {
-                Self::parenthesize(expr.operator.lexeme, [&expr.right].to_vec())
+                Self::parenthesize(&expr.operator.lexeme, [&expr.right].to_vec())
             }
         }
     }
-    // fn visit_binary_expr(expr: Binary) -> String {
-    //     return Self::parenthesize(
-    //         self,
-    //         expr.operator.lexeme,
-    //         [&expr.left, &expr.right].to_vec(),
-    //     );
-    // }
-    // fn visit_grouping_expr(expr: Grouping) -> String {
-    //     return Self::parenthesize(self, "group".to_string(), [&expr.expression].to_vec());
-    // }
-    // fn visit_literal_expr(expr: Literal) -> String {
-    //     match expr.value {
-    //         LiteralType::Nil => "nil".to_string(),
-    //         _ => expr.value.to_string(),
-    //     }
-    // }
-    // fn visit_unary_expr(expr: Unary) -> String {
-    //     return Self::parenthesize(self, expr.operator.lexeme, [&expr.right].to_vec());
-    // }
-    fn parenthesize(name: String, exprs: Vec<&Box<ExpressionType>>) -> String {
+    fn parenthesize(name: &String, exprs: Vec<&Box<ExpressionType>>) -> String {
         let mut builder = String::new();
 
         builder.push('(');
@@ -47,26 +30,8 @@ impl AstPrinter {
 
         for expr in exprs {
             builder.push(' ');
-            let expression_type = expr.as_ref();
-            match expression_type {
-                ExpressionType::LiteralExpr(lit_expr) => {
-                    match lit_expr.value.clone() {
-                        LiteralType::Bool(value) => {
-                            builder.push_str(value.to_string().as_str());
-                        }
-                        LiteralType::String(value) => {
-                            builder.push_str(value.as_str());
-                        }
-                        LiteralType::F32(value) => {
-                            builder.push_str(value.to_string().as_str());
-                        }
-                        LiteralType::Nil => {}
-                    }
-                }
-                ExpressionType::BinaryExpr(bin_expr) => {}
-                ExpressionType::GroupingExpr(grp_expr) => {}
-                ExpressionType::UnaryExpr(un_expr) => {}
-            }
+            let sub_expr = Self::print(&**expr);
+            builder.push_str(sub_expr.as_str());
         }
         builder.push(')');
         return builder;
--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,101 @@
-use std::{env, fs, io, io::Write, process};
 use jlox_rustport::{ast_printer, expr, parser, scanner, token_type};
+use std::{env, fs, io, io::Write, process};
 
 use expr::{Binary, ExpressionType, Grouping, Literal, Unary};
-use token_type::{LiteralType, Token, TokenType};
 use parser::Parser;
 use scanner::Scanner;
+use token_type::{LiteralType, Token, TokenType};
 struct Main {
     args: Vec<String>,
     had_error: bool,
 }
 
- impl Main {
-     fn main(mut self) {
-        //   let args_length = self.args.len();
-        //   if args_length < 2 || args_length > 3 {
-        //       println!("Usage: jlox [script]");
-        //       process::exit(64);
-        //   } else if args_length == 3 {
-        //       Self::run_file(&mut self);
-        //   } else if args_length == 2 {
-        //       Self::run_prompt(self);
-        //   }
-         let expr = Binary {
-             left: Box::new(ExpressionType::UnaryExpr(Unary {
-                 operator: Token {
-                     lexeme: "-".to_string(),
-                     literal: LiteralType::Nil,
-                     line: 1,
-                     ttype: TokenType::Minus,
-                 },
-                 right: Box::new(ExpressionType::LiteralExpr(Literal {
-                     value: LiteralType::F32(123 as f32),
-                 })),
-             })),
-             operator: Token {
-                 lexeme: "*".to_string(),
-                 line: 1,
-                 literal: LiteralType::Nil,
-                 ttype: TokenType::Star,
-             },
-             right: Box::new(ExpressionType::GroupingExpr(Grouping {
-                 expression: Box::new(ExpressionType::LiteralExpr(Literal {
-                     value: LiteralType::F32(45.67),
-                 })),
-             })),
-         };
+impl Main {
+    fn main(mut self) {
+        let args_length = self.args.len();
+        if args_length < 2 || args_length > 3 {
+            println!("Usage: jlox [script]");
+            process::exit(64);
+        } else if args_length == 3 {
+            Self::run_file(&mut self);
+        } else if args_length == 2 {
+            Self::run_prompt(self);
+        }
+        //  let expr = Binary {
+        //      left: Box::new(ExpressionType::UnaryExpr(Unary {
+        //          operator: Token {
+        //              lexeme: "-".to_string(),
+        //              literal: LiteralType::Nil,
+        //              line: 1,
+        //              ttype: TokenType::Minus,
+        //          },
+        //          right: Box::new(ExpressionType::LiteralExpr(Literal {
+        //              value: LiteralType::F32(123 as f32),
+        //          })),
+        //      })),
+        //      operator: Token {
+        //          lexeme: "*".to_string(),
+        //          line: 1,
+        //          literal: LiteralType::Nil,
+        //          ttype: TokenType::Star,
+        //      },
+        //      right: Box::new(ExpressionType::GroupingExpr(Grouping {
+        //          expression: Box::new(ExpressionType::LiteralExpr(Literal {
+        //              value: LiteralType::F32(45.67),
+        //          })),
+        //      })),
+        //  };
+    }
+    fn run_file(&self) {
+        if self.had_error {
+            process::exit(65);
+        } else {
+            let file = fs::read_to_string(&self.args[2]).expect("File reading successful");
+            let scanner: Scanner = Scanner {
+                source: file,
+                tokens: Vec::new(),
+                start: 0,
+                current: 0,
+                line: 1,
+            };
+            let scanned_tokens = scanner.scan_tokens();
+            let mut parser = Parser::new(scanned_tokens);
+            let expr = parser.parse();
+            println!(
+                "{:?}",
+                ast_printer::AstPrinter::print(&ExpressionType::BinaryExpr(expr))
+            )
+        }
+    }
 
-         println!(
-             "{:?}",
-             ast_printer::AstPrinter::print(ExpressionType::BinaryExpr(expr))
-         )
-     }
-     fn run_file(&self) {
-         if self.had_error {
-             process::exit(65);
-         } else {
-             let file = fs::read_to_string(&self.args[2]).expect("File reading successful");
-             let scanner: Scanner = Scanner {
-                 source: file,
-                 tokens: Vec::new(),
-                 start: 0,
-                 current: 0,
-                 line: 1,
-             };
-             let scanned_tokens = scanner.scan_tokens();
-             let mut parser = Parser::new(scanned_tokens);
-             let expr = parser.parse();
-             println!("{:?}", expr);
-         }
-     }
+    fn run_prompt(mut self) -> io::Error {
+        let mut input = String::new();
+        let mut std_out_handler: io::Stdout = io::stdout();
 
-     fn run_prompt(mut self) -> io::Error {
-         let mut input = String::new();
-         let mut std_out_handler: io::Stdout = io::stdout();
+        loop {
+            let _ = std_out_handler.write_all("> ".as_bytes());
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    Self::run(&input, &mut std_out_handler);
+                    self.had_error = false;
+                }
+                //Not sure if ctrl+D, to exit the program, generates an error response
+                Err(error) => return error,
+            }
+        }
+    }
 
-         loop {
-             let _ = std_out_handler.write_all("> ".as_bytes());
-             match io::stdin().read_line(&mut input) {
-                 Ok(_) => {
-                     Self::run(&input, &mut std_out_handler);
-                     self.had_error = false;
-                 }
-                 //Not sure if ctrl+D, to exit the program, generates an error response
-                 Err(error) => return error,
-             }
-         }
-     }
-
-     fn run(source: &String, std_out_handler: &mut io::Stdout) {
-          source.split(" ").for_each(|token| {
-              let _ = std_out_handler.write_all(token.as_bytes());
-          });
-     }
- }
+    fn run(source: &String, std_out_handler: &mut io::Stdout) {
+        source.split(" ").for_each(|token| {
+            let _ = std_out_handler.write_all(token.as_bytes());
+        });
+    }
+}
 
 fn main() {
     let main = Main {
         args: env::args().collect(),
         had_error: false,
     };
-     main.main();
+    main.main();
 }

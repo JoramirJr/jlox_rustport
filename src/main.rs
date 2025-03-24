fcf8741505b1c9590e78c4e2f89391add24f7ace
JoramirJr <joramirjunior@gmail.com>
2025-03-24 18:30:16 -0300

    fixing issues in the parser and lox structs (2)

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,22 @@ use std::env;
 use lox::Lox;
 
 pub fn main() {
-    let main = Lox {
+    let lox = Lox {
         args: env::args().collect(),
         had_error: false,
     };
-    main.start();
+    lox.start();
 }
+
+// impl Config {
+//     pub fn update_value(new_value: i32) {
+//         let mut config = CONFIG.lock().unwrap();
+//         config.value = new_value; // ✅ Mutate global state
+//     }
+
+//     pub fn get_value() -> i32 {
+//         CONFIG.lock().unwrap().value
+//     }
+// }
+
+// pub static CONFIG: Lazy<Mutex<Config>> = Lazy::new(|| Mutex::new(Config { value: 42 }));

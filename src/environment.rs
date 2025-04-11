a04ae36f1efa8cb979e0dc795673404d58feca04
JoramirJr <joramirjunior@gmail.com>
2025-04-11 12:39:16 -0300

    structuring variables environments

--- src/environment.rs
+++ src/environment.rs
@@ -1,13 +1,30 @@
 use std::collections::HashMap;
 
-use crate::token_type::LiteralType;
+use crate::{
+    interpreter::RuntimeError,
+    token_type::{LiteralType, Token},
+};
+
+type VarValue = Option<LiteralType>;
 
 struct Environment {
-    map: HashMap<String, Option<LiteralType>>,
+    values: HashMap<String, VarValue>,
 }
 
 impl Environment {
     fn define(&mut self, name: String, value: Option<LiteralType>) -> () {
-        self.map.insert(name, value);
+        self.values.insert(name, value);
+    }
+    fn get(&self, name: &Token) -> Result<VarValue, RuntimeError> {
+        let map_value = self.values.get(name.lexeme.as_str()).unwrap();
+
+        if let &None = map_value {
+            return Err(RuntimeError {
+                token: *name,
+                message: &format!("Undefied variable '{}'.", *name.lexeme),
+            });
+        } else {
+            return Ok(*map_value);
+        }
     }
 }
